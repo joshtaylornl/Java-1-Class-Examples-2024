4bdce32c4b5aade21107a6b327c0299f27ee001b
Josh Taylor <josh.taylor@K202Domain.CSN.com>
2024-09-11 10:26:43 -0230

    Initial commit

---
+++ src/main/java/ca/nl/cna/java1/helloworld/HelloWorld.java
@@ -0,0 +1,8 @@
+package ca.nl.cna.java1.helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/ca/nl/cna/java1/helloworld/HelloWorldSimple.java
@@ -0,0 +1,7 @@
+package ca.nl.cna.java1.helloworld;
+
+public class HelloWorldSimple {
+    void main() {
+        System.out.println("Hello World!");
+    }
+}
