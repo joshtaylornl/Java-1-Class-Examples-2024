69d476b18ffa5c61b6f9b61ad49a94f5cbedea47
josh.taylor <jktylr@gmail.com>
2024-10-02 12:23:07 -0230

    Updating code end of lecture on 2nd October.

---
+++ src/main/java/ca/nl/cna/java1/helloworld/FunWithConsoleInput.java
@@ -0,0 +1,30 @@
+package ca.nl.cna.java1.helloworld;
+
+import java.util.Scanner;
+
+/**
+ * In this example we will use simple console input from your keyboard
+ * using a Scanner object to read integers
+ *
+ * @author josh.taylor
+ */
+public class FunWithConsoleInput {
+
+    public static void main(String[] args) {
+        //Create a scanner object pointed at System.in which is the cursor on the console
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a number: ");
+        int x = input.nextInt();
+
+        System.out.println("Please enter a second number: ");
+        int y = input.nextInt();
+
+        int sum = x + y;
+
+        //Printing out variable sum in a string. Note: %d for integers!
+        System.out.printf("\nSum value is %d",  sum);
+
+    }
+
+}
