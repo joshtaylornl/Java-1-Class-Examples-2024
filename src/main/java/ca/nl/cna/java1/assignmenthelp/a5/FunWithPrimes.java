9ad1e9b9d7b609762224527b7aaaae86502140c0
josh.taylor <jktylr@gmail.com>
2024-11-21 15:57:33 -0330

    Updated based on 21 Nov with quick prime solution.

--- src/main/java/ca/nl/cna/java1/assignmenthelp/a5/FunWithPrimes.java
+++ src/main/java/ca/nl/cna/java1/assignmenthelp/a5/FunWithPrimes.java
@@ -1,18 +1,30 @@
 package ca.nl.cna.java1.assignmenthelp.a5;
 
+import java.util.Random;
+
 public class FunWithPrimes {
 
     public static void main(String[] args) {
         System.out.println("Fun With Primes");
 
         System.out.print("Number\tFactors\t\tPrime?");
-        //Checking the first 25 numbers for prime
-        for (int i = 2; i < 25; i++) {
-            System.out.printf("\n%d:\t\t %s\t\t\t %b", i,
-                    PrimeNumberCalculator.getUniquePrimeFactorization(i),
-                    PrimeNumberCalculator.isPrime(i));
+
+        Random random = new Random();
+        //10 large numbers
+        for (int i = 0; i < 100; i++) {
+            printNumberDetails(random.nextInt(2, Integer.MAX_VALUE));
         }
 
+    }
+
+    /**
+     * Print out the numbers information
+     * @param n
+     */
+    public static void printNumberDetails(int n){
+        System.out.printf("\n%d:\t\t %40s\t %b", n,
+                PrimeNumberCalculator.getUniquePrimeFactorization(n),
+                PrimeNumberCalculator.isPrime(n));
     }
 
 }
--- src/main/java/ca/nl/cna/java1/assignmenthelp/a5/PrimeNumberCalculator.java
+++ src/main/java/ca/nl/cna/java1/assignmenthelp/a5/PrimeNumberCalculator.java
@@ -6,31 +6,62 @@ package ca.nl.cna.java1.assignmenthelp.a5;
 public class PrimeNumberCalculator {
 
     /**
-     * //TODO write a cool code algorithm
-     * @param n
-     * @return
+     * Determine if a number is prime. (Returns false if less than or equal to 1)
+     * @param n number
+     * @return true if prime
      */
     public static boolean isPrime(int n){
         if(n <= 1){
             return false;
         }
-        if(n == 2){
-            return true;
-        }
-
-
-        //TODO Write this method
-        return false;
+        return n == getSmallestDivisor(n);
     }
 
     /**
-     * //TODO write a cool alg explainer
-     * @param n
-     * @return
+     * Generate a unique product of primes for any number. Seperated by a space character
+     * @param n number
+     * @return string of primes seperated by a space
      */
     public static String getUniquePrimeFactorization(int n){
-        //TODO Write this method
-        return String.valueOf(n);
+        int i = getSmallestDivisor(n);
+   /*
+        Ternary below is the same as this:
+        if(i == n){ //It's prime and we are done
+            return String.valueOf(i);
+        } else { //It's not prime, add the divisor to the string and repeat
+            return String.valueOf(i) + " " + getUniquePrimeFactorization(n/i);
+        }
+   */
+        return i == n ? String.valueOf(i) : String.valueOf(i) + " " + getUniquePrimeFactorization(n/i);
+    }
+
+    /**
+     * Get the smallest divisor of n. If n is prime then n will be the smallest divisor.
+     * @param n number
+     * @return smallest divisor
+     */
+    private static int getSmallestDivisor(int n){
+        if(n <= 1){ //keep this?
+            return n;
+        }
+        if(n % 2 == 0){ //check for even
+            return 2;
+        }
+        if(n % 3 == 0){
+            return 3;
+        }
+
+        //All the primes will be to the left or right of multiples of 6 (2*3)
+        for (int i = 6; i < Math.sqrt(n); i+=6) {
+            if(n % (i-1) == 0){
+                return i-1;
+            }
+            if(n % (i+1) == 0){
+                return i+1;
+            }
+        }
+
+        return n;
     }
 
 
